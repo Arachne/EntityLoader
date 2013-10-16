d782e090eae307f23566f635943f8c7b47bf16c3
Jáchym Toušek <enumag@gmail.com>
2013-10-17 00:07:31 +0200

    Updated tests runner

--- tests/runtests.bat
+++ tests/runtests.bat
@@ -1,7 +1,6 @@
 @ECHO OFF
 
 call "vendor/bin/phpcs.bat" -p --standard=vendor/arachne/coding-style/ruleset.xml src
-call "vendor/bin/phpcs.bat" -p --standard=vendor/arachne/coding-style/ruleset.xml tests/unit
-call "vendor/bin/phpcs.bat" -p --standard=vendor/arachne/coding-style/ruleset.xml tests/integration
+call "vendor/bin/phpcs.bat" -p --standard=vendor/arachne/coding-style/ruleset.xml --ignore=_temp tests
 call "vendor/bin/codecept.bat" build
 call "vendor/bin/codecept.bat" run
