2e0cdce97778fa5c2b23771da5cdd97d97bfc62f
Jáchym Toušek <enumag@gmail.com>
2013-10-16 21:34:17 +0200

    Removed debug option from codeception runner

--- tests/runtests.bat
+++ tests/runtests.bat
@@ -4,4 +4,4 @@ call "vendor/bin/phpcs.bat" -p --standard=vendor/arachne/coding-style/ruleset.xm
 call "vendor/bin/phpcs.bat" -p --standard=vendor/arachne/coding-style/ruleset.xml tests/unit
 call "vendor/bin/phpcs.bat" -p --standard=vendor/arachne/coding-style/ruleset.xml tests/integration
 call "vendor/bin/codecept.bat" build
-call "vendor/bin/codecept.bat" run -d
+call "vendor/bin/codecept.bat" run
