24de68c0a160a5b72f4f0b853829ae588866b0f6
Jáchym Toušek <enumag@gmail.com>
2015-02-14 19:46:56 +0100

    Fixed Route to work with non-array metadata

--- src/Application/Route.php
+++ src/Application/Route.php
@@ -11,6 +11,7 @@
 namespace Arachne\EntityLoader\Application;
 
 use Nette\Application\Routers\Route as BaseRoute;
+use Nette\InvalidArgumentException;
 
 /**
  * @author Jáchym Toušek <enumag@gmail.com>
@@ -23,6 +24,24 @@ class Route extends BaseRoute
 	 */
 	public function __construct($mask, $metadata = [], $flags = 0)
 	{
+		// Copied from Nette\Application\Routers\Route::__construct()
+		if (is_string($metadata)) {
+			$pos = strrpos($metadata, ':');
+			if (!$pos) {
+				throw new InvalidArgumentException("Second argument must be array or string in format Presenter:action, '$metadata' given.");
+			}
+			$metadata = [
+				self::PRESENTER_KEY => substr($metadata, 0, $pos),
+				'action' => $a === strlen($metadata) - 1 ? NULL : substr($metadata, $pos + 1),
+			];
+		} elseif ($metadata instanceof \Closure || $metadata instanceof Nette\Callback) {
+			$metadata = array(
+				self::PRESENTER_KEY => 'Nette:Micro',
+				'callback' => $metadata,
+			);
+		}
+
+		// Filter for handling Envelopes correctly
 		$filter = function (array $parameters) use ($metadata) {
 			foreach ($parameters as $key => & $value) {
 				if ($value instanceof Envelope && !isset($metadata[$key][self::FILTER_OUT])) {
@@ -32,6 +51,7 @@ class Route extends BaseRoute
 			return $parameters;
 		};
 
+		// Hack to invoke the filter after custom global filter
 		if (isset($metadata[NULL][self::FILTER_OUT])) {
 			$custom = $metadata[NULL][self::FILTER_OUT];
 			$metadata[NULL][self::FILTER_OUT] = function (array $parameters) use ($custom, $filter) {
