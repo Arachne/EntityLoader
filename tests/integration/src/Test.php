da4cfc470a88aefcb473d98c198717a25dc8e583
Jáchym Toušek <enumag@gmail.com>
2015-04-08 16:34:59 +0200

    Updated tests configuration

--- tests/integration/src/Test.php
+++ tests/integration/src/Test.php
@@ -4,6 +4,7 @@ namespace Tests\Integration;
 
 use Arachne\Codeception\ConfigFilesInterface;
 use Codeception\TestCase\Test as BaseTest;
+use Nette\Bridges\Framework\NetteExtension;
 
 /**
  * @author Jáchym Toušek <enumag@gmail.com>
@@ -14,7 +15,7 @@ abstract class Test extends BaseTest implements ConfigFilesInterface
 	public function getConfigFiles()
 	{
 		return [
-			'config/config.neon',
+			'config/config.neon' => class_exists(NetteExtension::class) ? 'nette_2.2' : 'nette_2.3',
 		];
 	}
 
