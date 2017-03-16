d7b87543424b425dc6d5ff282b291e891cfae398
Jáchym Toušek <enumag@gmail.com>
2017-03-16 19:41:38 +0100

    Remove copyright comments

--- src/Application/RequestEntityUnloader.php
+++ src/Application/RequestEntityUnloader.php
@@ -1,13 +1,5 @@
 <?php
 
-/*
- * This file is part of the Arachne package.
- *
- * Copyright (c) Jáchym Toušek (enumag@gmail.com)
- *
- * For the full copyright and license information, please view the file license.md that was distributed with this source code.
- */
-
 namespace Arachne\EntityLoader\Application;
 
 use Arachne\EntityLoader\EntityUnloader;
